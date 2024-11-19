4d67e70684464e4f7b66f05c51be9663b3901ea3
ShanmugarajDeveloper <97360276+ShanmugarajDeveloper@users.noreply.github.com>
2024-11-19 21:49:44 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿public class Singleton
+{
+    private static Singleton _instance;
+    private Singleton() { }
+
+    public static Singleton instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = new Singleton();
+            }
+            return _instance;
+        }
+        
+    }
+
+    public void DisplayMessage()
+    {
+        Console.WriteLine("Singleton Instance");
+    }
+}
+
+// Code that violates Singleton pattern
+public class CorrectedSingletonExample
+{
+    public static void Main()
+    {
+        // Incorrect way: New instances can still be created outside the Singleton class
+        Singleton singleton2 = Singleton.instance;
+        singleton2.DisplayMessage();
+        singleton2.DisplayMessage();
+    }
+}
